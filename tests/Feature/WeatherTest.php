23c464a0c1180efb281dc009c37e5d28c231ad32
Sabyrzhan Aspombitov <sabyrzhan.aspombitov@corp.oft-e.com>
2022-04-16 23:09:39 +0700

    added tests

---
+++ tests/Feature/WeatherTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Support\Facades\Http;
+use Tests\TestCase;
+
+class WeatherTest extends TestCase
+{
+    /**
+     * @dataProvider getDatSet
+     */
+    public function test_app_returns_responses(array $request, array $responses, array $expected, int $statusCode): void
+    {
+        $this->mockHttpClient($responses);
+        $response = $this->get('/api/current?' . http_build_query($request));
+        $response->assertStatus($statusCode);
+        $response->assertJson($expected);
+    }
+
+    private function mockHttpClient(array $responses): void
+    {
+        $stub = Http::fakeSequence();
+        foreach ($responses as $response) {
+            $stub->push($response);
+        }
+    }
+
+    public function getDatSet(): \Generator
+    {
+        $successResponse = [
+            'description' => 'ясно',
+            'temp' => 16,
+            'pressure' => 1023,
+            'humidity' => 65,
+            'wind' => ['direction' => 'северный', 'speed' => 2],
+        ];
+        $weatherData = [
+            'weather' => [['description' => 'ясно']],
+            'main' => ['temp' => 16.3, 'pressure' => 1023, 'humidity' => 65],
+            'wind' => ['speed' => 1.8, 'deg' => 350],
+        ];
+        yield [
+            'request' => ['city' => 'tomsk'],
+            'stubs' => [
+                [['lon' => 1.1, 'lat' => 1.1]],
+                $weatherData,
+            ],
+            'expected' => $successResponse,
+            'statusCode' => 200,
+        ];
+        yield [
+            'request' => ['latitude' => 1.1, 'longitude' => 1.1],
+            'stubs' => [
+                $weatherData,
+            ],
+            'expected' => $successResponse,
+            'statusCode' => 200,
+        ];
+        yield [
+            'request' => ['city' => 'unknown'],
+            'stubs' => [],
+            'expected' => ['message' => 'not found'],
+            'statusCode' => 404,
+        ];
+        yield [
+            'request' => ['city' => 'tomsk', 'units' => 'some'],
+            'stubs' => [],
+            'expected' => ['message' => 'An units should be either \'metric\' or \'imperial\''],
+            'statusCode' => 422,
+        ];
+    }
+}
---
+++ tests/Unit/DTO/WindTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Unit\DTO;
+
+use App\DTO\Wind;
+use PHPUnit\Framework\TestCase;
+
+class WindTest extends TestCase
+{
+    /**
+     * @dataProvider getDataForTest
+     */
+    public function test_getting_wind_direction_name_by_degree(float $degree, string $name): void
+    {
+        $wind = new Wind(25, $degree);
+        self::assertEquals($name, $wind->direction->value);
+    }
+
+    public function getDataForTest(): array
+    {
+        return [
+            ['degree' => 0, 'name' => 'северный'],
+            ['degree' => 22.4, 'name' => 'северный'],
+            ['degree' => 22.5, 'name' => 'северо-восточный'],
+            ['degree' => 45, 'name' => 'северо-восточный'],
+            ['degree' => 67.4, 'name' => 'северо-восточный'],
+            ['degree' => 67.5, 'name' => 'восточный'],
+            ['degree' => 90, 'name' => 'восточный'],
+            ['degree' => 112.4, 'name' => 'восточный'],
+            ['degree' => 112.5, 'name' => 'юго-восточный'],
+            ['degree' => 135, 'name' => 'юго-восточный'],
+            ['degree' => 157.4, 'name' => 'юго-восточный'],
+            ['degree' => 157.5, 'name' => 'южный'],
+            ['degree' => 180, 'name' => 'южный'],
+            ['degree' => 202.4, 'name' => 'южный'],
+            ['degree' => 202.5, 'name' => 'юго-западный'],
+            ['degree' => 225, 'name' => 'юго-западный'],
+            ['degree' => 247.4, 'name' => 'юго-западный'],
+            ['degree' => 247.5, 'name' => 'западный'],
+            ['degree' => 270, 'name' => 'западный'],
+            ['degree' => 292.4, 'name' => 'западный'],
+            ['degree' => 292.5, 'name' => 'северо-западный'],
+            ['degree' => 315, 'name' => 'северо-западный'],
+            ['degree' => 337.4, 'name' => 'северо-западный'],
+            ['degree' => 337.5, 'name' => 'северный'],
+            ['degree' => 360, 'name' => 'северный'],
+        ];
+    }
+}
